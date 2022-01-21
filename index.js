609800d948ebe85f90fc20f5fd354afa064547f3
unknown <juankliabaras@gmail.com>
2022-01-21 00:12:15 -0300

    primer commit calculadora creada localmente

---
+++ index.js
@@ -0,0 +1,48 @@
+const operaciones = require("./operaciones");
+
+function parsearArgumento(argv) {
+  objetoRespuesta = {};
+  if (argv.includes("+")) {
+    objetoRespuesta.operacion = "+";
+  }
+  if (argv.includes("-")) {
+    objetoRespuesta.operacion = "-";
+  }
+  if (argv.includes("/")) {
+    objetoRespuesta.operacion = "/";
+  }
+  if (argv.includes("*")) {
+    objetoRespuesta.operacion = "*";
+  }
+
+  const arrayNumeros = argv.split(objetoRespuesta.operacion);
+  const primerNumero = parseFloat(arrayNumeros[0]);
+  const segundoNumero = parseFloat(arrayNumeros[1]);
+
+  objetoRespuesta.primerTermino = primerNumero;
+  objetoRespuesta.segundoTermino = segundoNumero;
+
+  return objetoRespuesta;
+}
+
+function mapaOperaciones(objeto) {
+  const mapa = {
+    "+": operaciones.sumar,
+    "-": operaciones.restar,
+    "/": operaciones.dividir,
+    "*": operaciones.multiplicar,
+  };
+  var ejecutor = mapa[objeto.operacion];
+
+  return ejecutor(objeto.primerTermino, objeto.segundoTermino);
+}
+
+function main() {
+  const objetoArgumentos = parsearArgumento(process.argv[2]);
+
+  const respuesta = mapaOperaciones(objetoArgumentos);
+
+  console.log(respuesta);
+}
+
+main();
